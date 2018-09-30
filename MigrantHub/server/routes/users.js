c94d1d85800ccc7faeb0d87424abb7d7e87e6bb4
Laxman <laxman_20@hotmail.com>
2018-09-30 00:06:13 -0400

    Added eslint: Airbnb JavaScript Style Guide. Uses airbnb airbnb-base for the server side code and airbnb(included react plugins) for client side code.

--- MigrantHub/server/routes/users.js
+++ MigrantHub/server/routes/users.js
@@ -1,11 +1,12 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+
+const router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   res.json([
-    {id: 1, username: "somebody"},
-    {id: 2, username: "somebody_else"}
+    { id: 1, username: 'somebody' },
+    { id: 2, username: 'somebody_else' },
   ]);
 });
 
