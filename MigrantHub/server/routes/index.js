43bab9cd88276cf2ac1a14c49653658d64e2a209
rajee10 <rvairam10@gmail.com>
2018-10-08 12:07:01 -0400

    #4 Added passport and validation for user login

--- MigrantHub/server/routes/index.js
+++ MigrantHub/server/routes/index.js
@@ -2,8 +2,17 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', ensureAuthenticated, function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+function ensureAuthenticated(req, res, next){
+	if(req.isAuthenticated()){
+		return next();
+	} else {
+		//req.flash('error_msg','You are not logged in');
+		res.redirect('/users/login');
+	}
+}
 
 module.exports = router;
