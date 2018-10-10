1bc406ed100f6c2694c542ce19acff9212302663
Tajbid Choudhury <tajbidhussain@hotmail.com>
2018-10-09 23:35:21 -0400

    removed link for Main page. only available through login

--- MigrantHub/client/src/account/AccountTypeMenu.js
+++ MigrantHub/client/src/account/AccountTypeMenu.js
@@ -6,7 +6,6 @@ class AccountTypeMenu extends Component {
     return (
       <div>
         <ul>
-          <li><NavLink to="/"> Home </NavLink></li>
           <li><NavLink to="/UserAccount"> Create personal account </NavLink></li>
           <li><NavLink to="/BusinessAccount"> Create a business account </NavLink></li>
           <li><NavLink to="/Login">LogIn</NavLink></li>
