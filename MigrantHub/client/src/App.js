7c9a25d387ba0ac4019812a08294962129bf3123
Alexandre Masmoudi <a-masmoudi@hotmail.com>
2018-10-20 16:25:38 -0400

    Basic layout and infrastructure for events #11

--- MigrantHub/client/src/App.js
+++ MigrantHub/client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Routes from './routes/Routes';
 
+
 class App extends Component {
   render() {
     return (
--- MigrantHub/client/src/events/CreateEvent.js
+++ MigrantHub/client/src/events/CreateEvent.js
@@ -1,6 +1,22 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+<<<<<<< HEAD
 import validator from 'validator';
+=======
+import withStyles from '@material-ui/core/styles/withStyles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Paper from '@material-ui/core/Paper';
+import Stepper from '@material-ui/core/Stepper';
+import Step from '@material-ui/core/Step';
+import StepLabel from '@material-ui/core/StepLabel';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+import axios from 'axios';
+import { create } from 'jss';
+>>>>>>> Basic layout and infrastructure for events #11
 
 const visibilities = [
     { value: 'public', label: 'Public' },
@@ -23,7 +39,11 @@ const provinces = [
     { value: 'YT', label: 'Yukon' }
   ];
 
+<<<<<<< HEAD
 const repeats = [
+=======
+const repeatble = [
+>>>>>>> Basic layout and infrastructure for events #11
     { value: 'no', label: 'No' },
     { value: 'day', label: 'Daily' },
     { value: 'week', label: 'Weekly' },
@@ -56,6 +76,7 @@ class CreatEvent extends Component {
         dateStart: '',
         dateEnd:'',
         timeStart: '',
+<<<<<<< HEAD
         secondsStart: '',
         timeEnd: '',
         secondsEnd: '',
@@ -75,11 +96,16 @@ class CreatEvent extends Component {
         timeStartError: '',
         timeEndError: '',
         repeatError: '',
+=======
+        timeEnd: '',
+        repeatable: '', //No, Daily, Weekly, Monthly, Annualy
+>>>>>>> Basic layout and infrastructure for events #11
 
         // DB response
         messageFromServer: ''
     }
 
+<<<<<<< HEAD
     validate = () => {
         console.log("INSIDE VALIDATE()");
         let isError = false;
@@ -190,10 +216,13 @@ class CreatEvent extends Component {
         return isError;
     }
 
+=======
+>>>>>>> Basic layout and infrastructure for events #11
     handleChange = event => {
         this.setState({
           [event.target.name]: event.target.value
         })
+<<<<<<< HEAD
     }
 
     handleNext = () => {
@@ -233,6 +262,43 @@ class CreatEvent extends Component {
                     label="Visibility"
                     value={this.state.visibility} 
                     onChange={event => this.handleChange(event)}>
+=======
+      }
+
+    render() {
+
+        const { classes } = this.props;
+
+        const handleChange = this.props.handleChange;
+        const visibility = this.props.visibility;
+        const eventName = this.props.firstName;
+        const description = this.props.lastName;
+        const address = this.props.address;
+        const apartment = this.props.apartment;
+        const city = this.props.city;
+        const province = this.props.province;
+        const postalCode = this.props.postalCode;
+        const phoneNumber = this.props.phoneNumber;
+        const dateStart = this.props.dateStart;
+        const dateEnd = this.props.dateEnd;
+        const timeStart = this.props.timeStart;
+        const timeEnd = this.props.timeEnd;
+        const repeatable = this.props.repeatable;
+
+        return (
+            //TODOS:
+            // - Handle Errors
+            // - Validate input data
+            // - Handle onChange
+            <div>
+                <h3>Event Information</h3>
+                <form method="POST" action="">
+
+                    <label>Visibility: </label>
+                    <select id="visibility" name="visibility" label="Visibility"
+                    value={this.state.visibility} placeholder={this.state.visibilityError} 
+                    onChange={event => this.handleChange(event)} fullWidth>
+>>>>>>> Basic layout and infrastructure for events #11
                         {visibilities.map(option => (
                         <option key={option.value} value={option.value}>
                             {option.label}
@@ -243,6 +309,7 @@ class CreatEvent extends Component {
                     <br></br>
 
                     <label>Event Name: </label>
+<<<<<<< HEAD
                     <input 
                     type="text"
                     id="eventName" 
@@ -252,10 +319,16 @@ class CreatEvent extends Component {
                     onChange={event => this.handleChange(event)} 
                     error={this.state.eventNameError.length > 0}/>
                     <label style={{color: 'red'}}>  {this.state.eventNameError}</label>
+=======
+                    <input type="text" id="eventName" name="eventName" label="Name"
+                    value={this.state.eventName} placeholder={this.state.eventNameError} 
+                    onChange={event => this.handleChange(event)} fullWidth />
+>>>>>>> Basic layout and infrastructure for events #11
                     <br></br>
                     <br></br>
 
                     <label>Description: </label>
+<<<<<<< HEAD
                     <textarea 
                     id="description" 
                     name="description" 
@@ -264,10 +337,16 @@ class CreatEvent extends Component {
                     onChange={event => this.handleChange(event)} 
                     error={this.state.descriptionError.length > 0}/>
                     <label style={{color: 'red'}}>  {this.state.descriptionError}</label>
+=======
+                    <textarea id="description" name="description" label="Description" 
+                    value={this.state.description} placeholder={this.state.descriptionError} 
+                    onChange={event => this.handleChange(event)} fullWidth />
+>>>>>>> Basic layout and infrastructure for events #11
                     <br></br>
                     <br></br>
 
                     <label>Address: </label>
+<<<<<<< HEAD
                     <input 
                     type="text" 
                     id="address" 
@@ -278,10 +357,16 @@ class CreatEvent extends Component {
                     onChange={event => this.handleChange(event)}
                     error={this.state.addressError.length > 0}/>
                     <label style={{color: 'red'}}>  {this.state.addressError}</label>
+=======
+                    <input type="text" id="address" name="address" label="Address" 
+                    value={this.state.address} placeholder={this.state.addressError} 
+                    onChange={event => this.handleChange(event)}/>
+>>>>>>> Basic layout and infrastructure for events #11
                     <br></br>
                     <br></br>
 
                     <label>Apartment: </label>
+<<<<<<< HEAD
                     <input 
                     type="text" 
                     id="apartment" 
@@ -292,10 +377,16 @@ class CreatEvent extends Component {
                     onChange={event => this.handleChange(event)}
                     error={this.state.apartmentError.length > 0}/>
                     <label style={{color: 'red'}}>  {this.state.apartmentError}</label>
+=======
+                    <input type="text" id="apartment" name="apartment" label="Apartment" 
+                    value={this.state.apartment} placeholder={this.state.apartmentError} 
+                    onChange={event => this.handleChange(event)}/>
+>>>>>>> Basic layout and infrastructure for events #11
                     <br></br>
                     <br></br>
 
                     <label>City: </label>
+<<<<<<< HEAD
                     <input 
                     type="text" 
                     id="city" 
@@ -306,10 +397,16 @@ class CreatEvent extends Component {
                     onChange={event => this.handleChange(event)}
                     error={this.state.cityError.length > 0}/>
                     <label style={{color: 'red'}}>  {this.state.cityError}</label>
+=======
+                    <input type="text" id="city" name="city" label="City" 
+                    value={this.state.city} placeholder={this.state.cityError} 
+                    onChange={event => this.handleChange(event)}/>
+>>>>>>> Basic layout and infrastructure for events #11
                     <br></br>
                     <br></br>
 
                     <label>Province: </label>
+<<<<<<< HEAD
                     <select 
                     id="province" 
                     name="province" 
@@ -318,17 +415,26 @@ class CreatEvent extends Component {
                     placeholder={this.state.provinceError} 
                     onChange={event => this.handleChange(event)} 
                     error={this.state.provinceError.length > 0}>
+=======
+                    <select id="province" name="province" label="Province"
+                    value={this.state.province} placeholder={this.state.provinceError} 
+                    onChange={event => this.handleChange(event)} fullWidth>
+>>>>>>> Basic layout and infrastructure for events #11
                         {provinces.map(option => (
                         <option key={option.value} value={option.value}>
                             {option.label}
                         </option>
                         ))}
                     </select>
+<<<<<<< HEAD
                     <label style={{color: 'red'}}>  {this.state.provinceError}</label>
+=======
+>>>>>>> Basic layout and infrastructure for events #11
                     <br></br>
                     <br></br>
 
                     <label>Postal Code: </label>
+<<<<<<< HEAD
                     <input 
                     type="text" 
                     id="postalCode" 
@@ -340,10 +446,17 @@ class CreatEvent extends Component {
                     onChange={event => this.handleChange(event)}
                     error={this.state.postalCodeError.length > 0}/>
                     <label style={{color: 'red'}}>  {this.state.postalCodeError}</label>
+=======
+                    <input type="text" id="postalCode" name="postalCode" label="Postal Code" 
+                    value={this.state.postalCode} placeholder={this.state.postalCodeError}
+                    pattern="[/[a-zA-Z]/, /\d/, /[a-zA-Z]/, ' ', /\d/, /[a-zA-Z]/, /\d/]" 
+                    onChange={event => this.handleChange(event)}/>
+>>>>>>> Basic layout and infrastructure for events #11
                     <br></br>
                     <br></br>
                     
                     <label>Phone Number: </label>
+<<<<<<< HEAD
                     <input 
                     type="text" 
                     id="phoneNumber" 
@@ -355,10 +468,17 @@ class CreatEvent extends Component {
                     onChange={event => this.handleChange(event)}
                     error={this.state.phoneNumberError.length > 0}/>
                     <label style={{color: 'red'}}>  {this.state.phoneNumberError}</label>
+=======
+                    <input type="text" id="phoneNumber" name="phoneNumber" label="Phone Number" 
+                    value={this.state.phoneNumber} placeholder={this.state.phoneNumberError}
+                    pattern="['(', /[1-9]/, /\d/, /\d/, ')', ' ', /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/]" 
+                    onChange={event => this.handleChange(event)}/>
+>>>>>>> Basic layout and infrastructure for events #11
                     <br></br>
                     <br></br>
 
                     <label>Start Date: </label>
+<<<<<<< HEAD
                     <input 
                     type="date" 
                     id="dateStart" 
@@ -369,10 +489,16 @@ class CreatEvent extends Component {
                     onChange={event => this.handleChange(event)}
                     error={this.state.dateStart.length > 0}/>
                     <label style={{color: 'red'}}>  {this.state.dateStartError}</label>
+=======
+                    <input type="date" id="dateStart" name="dateStart" label="Start Date"
+                    value={this.state.dateStart} placeholder={this.state.dateStartError} 
+                    onChange={event => this.handleChange(event)} />
+>>>>>>> Basic layout and infrastructure for events #11
                     <br></br>
                     <br></br>
 
                     <label>End Date: </label>
+<<<<<<< HEAD
                     <input 
                     type="date" 
                     id="dateEnd" 
@@ -383,10 +509,16 @@ class CreatEvent extends Component {
                     onChange={event => this.handleChange(event)}
                     error={this.state.dateEnd.length > 0}/>
                     <label style={{color: 'red'}}>  {this.state.dateEndError}</label>
+=======
+                    <input type="date" id="dateEnd" name="dateEnd" label="End Date"
+                    value={this.state.dateEnd} placeholder={this.state.dateEndError} 
+                    onChange={event => this.handleChange(event)} />
+>>>>>>> Basic layout and infrastructure for events #11
                     <br></br>
                     <br></br>
 
                     <label>Start Time: </label>
+<<<<<<< HEAD
                     <input 
                     type="time" 
                     id="timeStart" 
@@ -397,10 +529,16 @@ class CreatEvent extends Component {
                     onChange={event => this.handleChange(event)}
                     error={this.state.timeStart.length > 0}/>
                     <label style={{color: 'red'}}>  {this.state.timeStartError}</label>
+=======
+                    <input type="time" id="timeStart" name="timeStart" label="Start Time"
+                    value={this.state.timeStart} placeholder={this.state.timeStartError}
+                    onChange={event => this.handleChange(event)} />
+>>>>>>> Basic layout and infrastructure for events #11
                     <br></br>
                     <br></br>
 
                     <label>End Time: </label>
+<<<<<<< HEAD
                     <input 
                     type="time" 
                     id="timeEnd" 
@@ -427,14 +565,32 @@ class CreatEvent extends Component {
                             {option.label}
                         </option>
                         ))}
+=======
+                    <input type="time" id="timeEnd" name="timeEnd" label="End Time"
+                    value={this.state.timeEnd} placeholder={this.state.timeEndError} 
+                    onChange={event => this.handleChange(event)}/>
+                    <br></br>
+                    <br></br>
+
+                    <label>Repeatable: </label>                   
+                    <select id="repeatable" name="repeatable" label="Repeatable"
+                    value={this.state.repeatable} placeholder={this.state.repeatableError} 
+                    onChange={event => this.handleChange(event)} fullWidth>
+                        <option value="no">No</option>
+                        <option value="daily">Daily</option>
+>>>>>>> Basic layout and infrastructure for events #11
                     </select>
                     <br></br>
                     <br></br>
 
+<<<<<<< HEAD
                     <input 
                     type="button" 
                     value="Create" 
                     onClick={this.handleNext}/>
+=======
+                    <input type="submit" value="Create"/>
+>>>>>>> Basic layout and infrastructure for events #11
                     <br></br>
                     <br></br>
 
@@ -448,5 +604,9 @@ CreatEvent.propTypes = {
     classes: PropTypes.object.isRequired,
   };
 
+<<<<<<< HEAD
 export default (CreatEvent);
 
+=======
+export default (CreatEvent);
+>>>>>>> Basic layout and infrastructure for events #11
