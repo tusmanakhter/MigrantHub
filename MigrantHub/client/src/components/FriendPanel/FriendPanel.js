a976a9b5e75b8a4cc80f845386d2e483bedb5772
Mira <miramarhaba@hotmail.com>
2018-10-25 22:15:01 -0400

    cleanup for FriendPanel

--- MigrantHub/client/src/components/FriendPanel/FriendPanel.js
+++ MigrantHub/client/src/components/FriendPanel/FriendPanel.js
@@ -8,52 +8,57 @@ import yes from './yes.svg';
 import no from './no.svg';
 
 class FriendPanel extends Component {
-    render(props) {
-      return (
-        <div>
-          <Card className="Card-friend-panel">
-            <CardContent>
-              <p>Your list of friends:</p>
-              <ul>
-                {this.props.friends.map( friend =>
-                  <li key={friend.id}><img src={friend.pic} alt="profile pic" className="User-avatar"/>{friend.name}</li>
-                )}
-              </ul>
-            </CardContent>
-          </Card>
-          <Card className="Card-friend-panel">
-            <CardContent>
-            <p>Your friend requests:</p>
-              <ul>
-                {this.props.friendRequests.map( request =>
-                  <li key={request.id}>
-                    <img src={request.pic} alt="profile pic" className="User-avatar"/>
-                    {request.name}
-                    <Button variant="contained" color="primary"><img src={yes} alt="yes"/></Button>
-                    <Button variant="contained" color="primary"><img src={no} alt="no"/></Button>
-                  </li>
-                )}
-              </ul>
-            </CardContent>
-          </Card>
-          <Card className="Card-friend-panel">
-            <CardContent>
-              <p>Add Friends:</p>
-              <TextField
-                id="outlined-name"
-                label="User's Email"
-                value=""
-                //TODO add handler
-                // onChange={this.handleChange('name')}
-                margin="normal"
-                variant="outlined"
-              />
-              <Button variant="contained" color="primary">Add Friend</Button>
-            </CardContent>
-          </Card>
-        </div>
-      );
-    }
+  render(props) {
+    return (
+      <div>
+        <Card className="Card-friend-panel">
+          <CardContent>
+            <p>Your list of friends:</p>
+            <ul>
+              {this.props.friends.map( friend =>
+                <li key={friend.id}><img src={friend.pic} alt="profile pic" className="User-avatar"/>{friend.name}</li>
+              )}
+            </ul>
+          </CardContent>
+        </Card>
+        <Card className="Card-friend-panel">
+          <CardContent>
+          <p>Your friend requests:</p>
+            <ul>
+              {this.props.friendRequests.map( request =>
+                <li key={request.id}>
+                  <img src={request.pic} alt="profile pic" className="User-avatar"/>
+                  {request.name}
+                  <Button variant="contained" color="primary"><img src={yes} alt="yes"/></Button>
+                  <Button variant="contained" color="primary"><img src={no} alt="no"/></Button>
+                </li>
+              )}
+            </ul>
+          </CardContent>
+        </Card>
+        <Card className="Card-friend-panel">
+          <CardContent>
+            <p>Add Friends:</p>
+            <TextField
+              id="outlined-name"
+              label="User's Email"
+              value=""
+              //TODO add handler
+              // onChange={this.handleChange('name')}
+              margin="normal"
+              variant="outlined"
+            />
+            <Button variant="contained" color="primary" onclick={this.handleAddFriend}>Add Friend</Button>
+          </CardContent>
+        </Card>
+      </div>
+    );
   }
+
+  handleAddFriend(event) {
+    
+
+  }
+}
 
   export default FriendPanel;
