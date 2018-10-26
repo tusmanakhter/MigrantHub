87c204144cd62b172f14c0bc4d3ae2d790606aac
Laxman <laxman_20@hotmail.com>
2018-10-25 21:38:33 -0400

    #10 Added ServiceController tests. Added a service factory that will return Service data for service tests to avoid duplication and make refactoring easier.

---
+++ MigrantHub/server/test/factories/ServicesFactory.js
@@ -0,0 +1,84 @@
+module.exports.validServiceData = function() {
+    return {
+        email : "test@hotmail.com",
+        serviceTitle : "My First Service",
+        serviceSummary : "In reasonable compliment favourable is connection dispatched in terminated.",
+        serviceDescription : "Way nor furnished sir procuring therefore but. Warmth far manner myself active are cannot called.",
+        serviceDate : {
+            startDate : "2018-11-01",
+            endDate : "2019-02-01"
+        },
+        location : {
+            address : "1455 Boulevard de Maisonneuve O",
+            apartment : "",
+            city : "Montreal",
+            province : "QC",
+            postalCode : "H3G 1M8",
+            phoneNumber : "(514) 848-2424"
+        },
+        serviceHours : [
+            {
+                serviceDay : "monday",
+                startTime : "08:00",
+                endTime : "17:00"
+            },
+        ],
+    };
+};
+
+module.exports.emptyServiceData = function() {
+    return {
+        email : "",
+        serviceTitle : "",
+        serviceSummary : "",
+        serviceDescription : "",
+        serviceImagePath : "",
+        serviceDate : {
+            startDate : "",
+            endDate : ""
+        },
+        location : {
+            address : "",
+            apartment : "",
+            city : "",
+            province : "",
+            postalCode : "",
+            phoneNumber : ""
+        },
+        serviceHours : [
+            {
+                serviceDay : "",
+                startTime : "",
+                endTime : ""
+            },
+        ],
+    };
+};
+
+module.exports.invalidServiceData = function() {
+    return {
+        email : "test@hotmail.com",
+        serviceTitle : "My First Service",
+        serviceSummary : "In reasonable compliment favourable is connection dispatched in terminated.",
+        serviceDescription : "Way nor furnished sir procuring therefore but. Warmth far manner myself active are cannot called.",
+        serviceDate : {
+            startDate : "2018-11-01",
+            endDate : "2019-02-01"
+        },
+        location : {
+            address : "1455 Boulevard de Maisonneuve O",
+            apartment : "",
+            city : "Montreal",
+            province : "QC",
+            postalCode : "H3G 1MP",
+            phoneNumber : "(514) 848-24AA"
+        },
+        serviceHours : [
+            {
+                serviceDay : "monday",
+                startTime : "088:00",
+                endTime : "178:00"
+            },
+        ],
+    };
+};
--- MigrantHub/server/test/validators/ServiceValidatorTest.js
+++ MigrantHub/server/test/validators/ServiceValidatorTest.js
@@ -1,90 +1,12 @@
 var ServiceValidator = require('../../validators/ServiceValidator');
 var chai = require('chai');
 var assert = chai.assert;
-
-const validServiceData = {
-    email : "test@hotmail.com",
-    serviceTitle : "My First Service",
-    serviceSummary : "In reasonable compliment favourable is connection dispatched in terminated.",
-    serviceDescription : "Way nor furnished sir procuring therefore but. Warmth far manner myself active are cannot called.",
-    serviceDate : {
-        startDate : "2018-11-01",
-        endDate : "2019-02-01"
-    },
-    location : {
-        address : "1455 Boulevard de Maisonneuve O",
-        apartment : "",
-        city : "Montreal",
-        province : "QC",
-        postalCode : "H3G 1M8",
-        phoneNumber : "(514) 848-2424"
-    },
-    serviceHours : [
-        {
-            serviceDay : "monday",
-            startTime : "08:00",
-            endTime : "17:00"
-        },
-    ],
-};
-
-const emptyServiceData = {
-    email : "",
-    serviceTitle : "",
-    serviceSummary : "",
-    serviceDescription : "",
-    serviceImagePath : "",
-    serviceDate : {
-        startDate : "",
-        endDate : ""
-    },
-    location : {
-        address : "",
-        apartment : "",
-        city : "",
-        province : "",
-        postalCode : "",
-        phoneNumber : ""
-    },
-    serviceHours : [
-        {
-            serviceDay : "",
-            startTime : "",
-            endTime : ""
-        },
-    ],
-};
-
-const invalidServiceData = {
-    email : "test@hotmail.com",
-    serviceTitle : "My First Service",
-    serviceSummary : "In reasonable compliment favourable is connection dispatched in terminated. Do esteem object we called father excuse remove. So dear real on like more it. Laughing for two families addition expenses surprise the. If sincerity he to curiosity arranging. Learn taken terms be as. Scarcely mrs produced too removing new old.   Kindness to he horrible reserved ye. Effect twenty indeed beyond for not had county. The use him without greatly can private. Increasing it unpleasant no of contrasted no continuing. Nothing colonel my no removed in weather. It dissimilar in up devonshire inhabiting. ",
-    serviceDescription : "Way nor furnished sir procuring therefore but. Warmth far manner myself active are cannot called. Set her half end girl rich met. Me allowance departure an curiosity ye. In no talking address excited it conduct. Husbands debating replying overcame blessing he it me to domestic. \n\nOf friendship on inhabiting diminution discovered as. Did friendly eat breeding building few nor. Object he barton no effect played valley afford. Period so to oppose we little seeing or branch. Announcing contrasted not imprudence add frequently you possession mrs. Period saw his houses square and misery. Hour had held lain give yet. \n\nHim boisterous invitation dispatched had connection inhabiting projection. By mutual an mr danger garret edward an. Diverted as strictly exertion addition no disposal by stanhill. This call wife do so sigh no gate felt. You and abode spite order get. Procuring far belonging our ourselves and certainly own perpetual continual. It elsewhere of sometimes or my certainty. Lain no as five or at high. Everything travelling set how law literature. ",
-    serviceDate : {
-        startDate : "2018-11-01",
-        endDate : "2019-02-01"
-    },
-    location : {
-        address : "1455 Boulevard de Maisonneuve O",
-        apartment : "",
-        city : "Montreal",
-        province : "QC",
-        postalCode : "H3G 1MP",
-        phoneNumber : "(514) 848-24AA"
-    },
-    serviceHours : [
-        {
-            serviceDay : "monday",
-            startTime : "088:00",
-            endTime : "178:00"
-        },
-    ],
-};
+var ServicesFactory = require('../factories/ServicesFactory');
 
 describe('ServiceValidator()', function () {
     it('Validate service data should return empty error string.', function () {
 
-        let errors = ServiceValidator(validServiceData);
+        let errors = ServiceValidator(ServicesFactory.validServiceData());
         assert.equal(errors, "");
     });
 });
@@ -107,7 +29,7 @@ describe('ServiceValidator()', function () {
             "\nService hour start time required and empty" +
             "\nService hour end time required and empty";
 
-        let forcedErrors = ServiceValidator(emptyServiceData);
+        let forcedErrors = ServiceValidator(ServicesFactory.emptyServiceData());
         assert.equal(forcedErrors, expectedErrors);
     });
 });
@@ -121,7 +43,7 @@ describe('ServiceValidator()', function () {
             "\nService hour start time should be in the format 13:57" +
             "\nService hour end time should be in the format 13:57";
 
-        let forcedErrors = ServiceValidator(invalidServiceData);
+        let forcedErrors = ServiceValidator(ServicesFactory.invalidServiceData());
         assert.equal(forcedErrors, expectedErrors);
     });
 });
