2e8fa6b4554f9c839d36138049d821f0bcce1ab9
Mira <miramarhaba@hotmail.com>
2018-10-27 03:17:54 -0400

    #5 - validating for existing duplicate friend requests

--- MigrantHub/server/validators/FriendRequestValidator.js
+++ MigrantHub/server/validators/FriendRequestValidator.js
@@ -1,3 +1,4 @@
+var FriendRequest = require('../models/FriendRequest');
 var validator = require('validator');
 
 // Function to perform server-side validation of the friend request before sending to db.
@@ -6,13 +7,31 @@ const FriendRequestValidator = (friendRequestObject) =>{
 
     if (validator.isEmpty(friendRequestObject.requestFrom)) {
         errors += "{'\n'}Problem with your session. Please log in again.";
-    } else if (validator.isEmpty(friendRequestObject.requestTo)) {
+    }
+    if (validator.isEmpty(friendRequestObject.requestTo)) {
         errors += "{'\n'}Empty friend request. Please input an email.";
     }
     if (!validator.isEmail(friendRequestObject.requestFrom)) {
         errors += "{'\n'}Problem with your session. Please log in again."
-    } else if (!validator.isEmail(friendRequestObject.requestTo)) {
+    }
+    if (!validator.isEmail(friendRequestObject.requestTo)) {
         errors += "{'\n'}Inputted friend to add is not a valid email.";
+    }
+
+    var recordExists = FriendRequest.find({
+        requestFrom: friendRequestObject.requestFrom,
+        requestTo: friendRequestObject.requestTo
+    })
+    if (recordExists) {
+        errors += "You have already sent this person a friend request.";
+    } else {
+        recordExists = FriendRequest.find({
+            requestFrom: friendRequestObject.requestTo,
+            requestTo: friendRequestObject.requestFrom
+        })
+        if (recordExists) {
+            errors += "This person has already sent you a friend request.";
+        }
     }
     return errors;
 };
