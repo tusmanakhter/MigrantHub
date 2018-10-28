964efc61524e6b7eaf8061a2a71d8b863ce8fab8
Alexandre Masmoudi <a-masmoudi@hotmail.com>
2018-10-28 18:57:30 -0400

    Added EventController testing and created Event Factory #11

--- MigrantHub/server/test/validators/CreateEventValidatorTest.js
+++ MigrantHub/server/test/validators/CreateEventValidatorTest.js
@@ -1,70 +1,11 @@
 var CreateEventValidator = require('../../validators/CreateEventValidator');
 var chai = require('chai');
 var assert = chai.assert;
-
-const validCreateEventData = {
-    creator: 'alex@alex.alex',
-    visibility: 'public',
-    eventName: 'My Awesome Event',
-    description: 'Awesome Event Description',
-    address: '777 Awesome',
-    apartment: '',
-    city: 'Las Vegas',
-    province: 'QC',
-    postalCode: 'H1Z 8P2',
-    phoneNumber: '(514) 123-1234',
-    dateStart: '2020-01-01',
-    dateEnd:'2021-01-01',
-    timeStart: '10:00',
-    secondsStart: '36000',
-    timeEnd: '22:00',
-    secondsEnd: '79200',
-    repeat: 'no', 
-};
-
-const emptyCreateEventData = {
-    creator: '',
-    visibility: '',
-    eventName: '',
-    description: '',
-    address: '',
-    apartment: '',
-    city: '',
-    province: '',
-    postalCode: '',
-    phoneNumber: '',
-    dateStart: '',
-    dateEnd:'',
-    timeStart: '',
-    secondsStart: '',
-    timeEnd: '',
-    secondsEnd: '',
-    repeat: '', 
-};
-
-const invalidNumbersCreateEventData = {
-    creator: 'alex@alex.alex',
-    visibility: 'public',
-    eventName: '123',
-    description: '123',
-    address: '123',
-    apartment: '',
-    city: '123',
-    province: 'QC',
-    postalCode: '213',
-    phoneNumber: '123',
-    dateStart: '2018-01-01',
-    dateEnd:'2017-01-01',
-    timeStart: '00:10',
-    secondsStart: '600',
-    timeEnd: '00:00',
-    secondsEnd: '0',
-    repeat: 'no', 
-};
+var EventFactory = require('../factories/EventFactory');
 
 describe('CreateEventValidator()', function () {
     it('Validates create event data should return empty error string.', function () {
-        let errors = CreateEventValidator(validCreateEventData);
+        let errors = CreateEventValidator(EventFactory.validCreateEventData());
         assert.equal(errors, "");
     });
 
@@ -84,7 +25,7 @@ describe('CreateEventValidator()', function () {
             "\nStart time is required" +
             "\nEnd time is required";
 
-        let forcedErrors = CreateEventValidator(emptyCreateEventData);
+        let forcedErrors = CreateEventValidator(EventFactory.emptyCreateEventData());
         assert.equal(forcedErrors, expectedErrors);
     });
 
@@ -99,7 +40,7 @@ describe('CreateEventValidator()', function () {
             "\nEnd date is invalid" +
             "\nEnd time is invalid";
 
-        let forcedErrors = CreateEventValidator(invalidNumbersCreateEventData);
+        let forcedErrors = CreateEventValidator(EventFactory.invalidNumbersCreateEventData());
         assert.equal(forcedErrors, expectedErrors);
     });
 });
