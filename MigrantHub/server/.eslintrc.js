75efa10bd1cba80a8960ea7843162bbb82fdedd4
Laxman <laxman_20@hotmail.com>
2018-11-08 12:17:49 -0500

    Add ESLint Required files, libraries and scripts to run.

---
+++ MigrantHub/server/.eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    "env": {
+        "node": true,
+        "mocha": true,
+    },
+    "extends": "airbnb-base",
+    "rules": {
+        "no-unused-vars": ["error", { "argsIgnorePattern": "next" }],
+        "linebreak-style": 0,
+        "indent": ["error", 2],
+        "no-underscore-dangle": 0,
+    }
+};
