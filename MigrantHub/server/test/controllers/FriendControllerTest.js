32e10e68a55b1b254608dbee45d5e3aa2e50f423
Tajbid Choudhury <tajbidhussain@hotmail.com>
2018-11-12 01:14:51 -0500

    #23 refactored tests, and added tests for new functionality.

--- MigrantHub/server/test/controllers/FriendControllerTest.js
+++ MigrantHub/server/test/controllers/FriendControllerTest.js
@@ -14,11 +14,16 @@ describe('friend controller friend management', function () {
       _id: "5bd33cb2e43f7f17bbe8422d"
     },
     user: {
-      _id: "test@test.com"
+      _id: "test@test.com",
+      _id2: "testFrom@test.com",
     },
     friendsList: [{
       friend_id: "testFrom@test.com",
       isFriend: true,
+    },
+    {
+      friend_id: "test1@test.com",
+      isFriend: false,
     }],
   },
     error = new Error({ error: "err" }),
@@ -41,19 +46,27 @@ describe('friend controller friend management', function () {
 
   it('should accept a friend request', test(function () {
     expectedResult = req.body
+    this.stub(MigrantUser, 'findOne').yields(null, expectedResult, req.user._id);
     this.stub(MigrantUser, 'update').yields(null, expectedResult, req.user._id);
     this.stub(FriendRequest, 'findByIdAndDelete').yields(req._id);
     Controller.acceptFriendRequest(req, res);
-    assert.calledWith(MigrantUser.update, { _id: "test@test.com" });
-    assert.calledWith(MigrantUser.update, { _id: "testFrom@test.com" });
+    assert.calledWith(MigrantUser.findOne, { _id: req.user._id, friendsList: { $elemMatch: { friend_id: expectedResult.requestFrom } } });
+    assert.calledWith(MigrantUser.update, { _id: req.user._id, 'friendsList.friend_id': expectedResult.requestFrom }, { $set: { 'friendsList.$.isFriend': true } });
+    assert.calledWith(MigrantUser.update, { _id: req.user._id2, 'friendsList.friend_id': expectedResult.requestTo }, { $set: { 'friendsList.$.isFriend': true } });
     assert.calledWith(FriendRequest.findByIdAndDelete, { _id: "5bd33cb2e43f7f17bbe8422d" });
-    assert.calledWith(res.send, "Friend has been accepted and removed from request friend table");
   }));
 
   it('should list all accepted friends', test(function () {
     expectedResult = req.friendsList
-    this.stub(MigrantUser, 'findOne').yields(null, expectedResult);
+    this.stub(MigrantUser, 'aggregate').yields(null, expectedResult, req.user._id);
     Controller.getFriendsList(req, res);
-    assert.calledWith(MigrantUser.findOne, { _id: "test@test.com" });
+    assert.calledWith(MigrantUser.aggregate, ([{ $match: { _id: req.user._id } }, { $project: { friendsList: { $filter: { input: '$friendsList', as: 'friends', cond: { $eq: ['$$friends.isFriend', true] } } } } }]));
+  }));
+
+  it('should set to false once deleted', test(function () {
+    expectedResult = req.friendsList
+    this.stub(MigrantUser, 'update').yields(null, expectedResult, req.user._id);
+    Controller.unfriend(req, res);
+    assert.calledWith(MigrantUser.update, { _id: req.user._id, 'friendsList.friend_id': expectedResult.friendId }, { $set: { 'friendsList.$.isFriend': false } });
   }));
 });
