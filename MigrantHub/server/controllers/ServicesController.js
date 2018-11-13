11d33b50b219e3bcf1f0e1562cb6aea8f5be54c9
Laxman <laxman_20@hotmail.com>
2018-11-12 23:52:33 -0500

    Add server side logging with a info level for prod and debug level for dev using winston. The format of the logs is similar to the apache log format, with fields timestamp, ip, method, url, httpVersion, status, referrer, customMessage, errorMessage.

--- MigrantHub/server/controllers/ServicesController.js
+++ MigrantHub/server/controllers/ServicesController.js
@@ -3,6 +3,7 @@ const multer = require('multer');
 const fs = require('fs-extra');
 const ServiceValidator = require('../validators/ServiceValidator');
 const Services = require('../models/Services');
+const { logger, formatMessage } = require('../config/winston');
 
 const multerStorage = multer.diskStorage({
   destination(req, file, cb) {
@@ -36,18 +37,23 @@ module.exports = {
       services.location = parsedObj.location;
       services.serviceHours = parsedObj.serviceHours;
       if (parsedObj.serviceImageName === 'cameraDefault.png') {
-        services.serviceImagePath = (`../default/${parsedObj.serviceImageName}`);
+          services.serviceImagePath = (`../default/${parsedObj.serviceImageName}`);
       } else {
-        services.serviceImagePath = (`../${req.user._id}/services/${parsedObj.serviceImageName}`);
+          services.serviceImagePath = (`../${req.user._id}/services/${parsedObj.serviceImageName}`);
       }
       services.dateCreated = date;
       services.save((err) => {
         if (err) {
+            logger.error(formatMessage(req.ip, req.method, req.originalUrl, req.httpVersion,
+                err.status, req.referer,'servicesController.createService' , err.message));
           return res.status(400).send('There was a error creating service.');
         }
         return res.status(200).send('Service has been created!');
       });
     } else {
+        logger.error(formatMessage(req.ip, req.method, req.originalUrl, req.httpVersion,
+            '500' , req.referer,'servicesController.createService: ServiceValidator' , errors));
+        return res.status(400).send('There was a error creating service.');
       return res.status(400).send('There was a error creating service.');
     }
   },
@@ -62,6 +68,8 @@ module.exports = {
 
     Services.find(query, (err, services) => {
       if (err) {
+          logger.error(formatMessage(req.ip, req.method, req.originalUrl, req.httpVersion,
+              err.status, req.referer,'servicesController.viewServices' , err.message));
         return res.status(400).send('There was a error getting services.');
       }
       return res.status(200).send(services);
@@ -78,6 +86,8 @@ module.exports = {
 
     Services.findOne(query, (err, services) => {
       if (err) {
+          logger.error(formatMessage(req.ip, req.method, req.originalUrl, req.httpVersion,
+              err.status, req.referer,'servicesController.getServiceData' , err.message));
         return res.status(400).send('There was a error getting services.');
       }
       return res.status(200).send(services);
@@ -104,6 +114,8 @@ module.exports = {
       if ((parsedObj.serviceImagePath !== undefined) && (parsedObj.serviceImagePath !== (`../${req.user._id}/services/${parsedObj.serviceImageName}`))) {
         fs.remove(`uploads/${parsedObj.serviceImagePath.toString().substring(3)}`, (err) => {
           if (err) {
+            logger.error(formatMessage(req.ip, req.method, req.originalUrl, req.httpVersion,
+                err.status, req.referer,'servicesController.updateService: removeImage' , err.message));
             updateError = true;
           }
         });
@@ -117,6 +129,8 @@ module.exports = {
 
       Services.findByIdAndUpdate({ _id: parsedObj._id }, parsedObj, { new: true }, (err) => {
         if (err) {
+          logger.error(formatMessage(req.ip, req.method, req.originalUrl, req.httpVersion,
+              err.status, req.referer,'servicesController.updateService' , err.message));
           updateError = true;
         }
       });
@@ -126,6 +140,8 @@ module.exports = {
       }
       return res.status(200).send('Service updated successfully.');
     }
+    logger.error(formatMessage(req.ip, req.method, req.originalUrl, req.httpVersion,
+        '400', req.referer,'servicesController.updateService' , errors));
     return res.status(400).send('There was an error updating service.');
   },
 
@@ -134,6 +150,8 @@ module.exports = {
     Services.updateOne({ _id: req.params.id },
       { deleted: true, deletedDate: Date.now() }, (err) => {
         if (err) {
+          logger.error(formatMessage(req.ip, req.method, req.originalUrl, req.httpVersion,
+              err.status, req.referer,'servicesController.deleteService' , err.message));
           deleteError = true;
         }
       });
