44e7f3f33c754aa7648fbf8a6595f09b5260c83e
Unknown <nahlemazen@gmail.com>
2019-01-03 21:51:43 -0500

    #118 New UI for event section

--- MigrantHub/client/src/events/ViewEvent.js
+++ MigrantHub/client/src/events/ViewEvent.js
@@ -1,4 +1,10 @@
 import React, { Component } from 'react';
+import {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  Marker
+} from "react-google-maps";
 import withStyles from '@material-ui/core/styles/withStyles';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
@@ -13,13 +19,56 @@ import { Redirect } from 'react-router-dom';
 import axios from 'axios';
 import GoogleMaps from 'components/GoogleMaps/GoogleMaps';
 import UserTypes from 'lib/UserTypes';
+// @material-ui/icons
+import Map from "@material-ui/icons/Map";
+import AddLocation from "@material-ui/icons/AddLocation";
+import Place from "@material-ui/icons/Place";
+import Dashboard from "@material-ui/icons/Dashboard";
+import Schedule from "@material-ui/icons/Schedule";
+import CardIcon from "components/Card/CardIcon.jsx";
+
+import GridItem from "components/Grid/GridItem.jsx";
+import NavPills from "components/NavPills/NavPills.jsx";
+import Card from "components/Card/Card.jsx";
+import CardHeader from "components/Card/CardHeader.jsx";
+import CardBody from "components/Card/CardBody.jsx";
+import { cardTitle } from "assets/jss/material-dashboard-pro-react.jsx";
 
 const styles = {
+  cardTitle,
+  pageSubcategoriesTitle: {
+    color: "#3C4858",
+    textDecoration: "none",
+    textAlign: "center"
+  },
+  cardCategory: {
+    margin: "0",
+    color: "#999999"
+  },
   avatar: {
     backgroundColor: blue[100],
     color: blue[600],
   },
+  cardIconTitle: {
+    ...cardTitle,
+    marginTop: "15px",
+    marginBottom: "0px"
+  }
 };
+
+const RegularMap = withScriptjs(
+  withGoogleMap(props => (
+    <GoogleMap
+      defaultZoom={8}
+      defaultCenter={{ lat: 40.748817, lng: -73.985428 }}
+      defaultOptions={{
+        scrollwheel: false
+      }}
+    >
+      <Marker position={{ lat: 40.748817, lng: -73.985428 }} />
+    </GoogleMap>
+  ))
+);
 
 class ViewEvent extends Component {
   constructor(props) {
@@ -68,7 +117,7 @@ class ViewEvent extends Component {
 
   render() {
     const {
-      eventName, description, dateStart, dateEnd, timeStart, timeEnd,
+      classes, eventName, description, dateStart, dateEnd, timeStart, timeEnd,
       location, open, scroll, onClose,
     } = this.props;
     const { redirectTo, redirectToURL, redirectState, type } = this.state;
@@ -88,124 +137,156 @@ class ViewEvent extends Component {
         <Dialog
           open={open}
           onClose={onClose}
+          style={{backgroundColor: 'transparent'}}
+          overlayStyle={{backgroundColor: 'transparent'}}
+          PaperProps={{
+            style: {
+              backgroundColor: 'transparent',
+              boxShadow: 'none',
+            },
+          }}
           scroll={scroll}
           aria-labelledby="scroll-dialog-title"
           fullWidth
-          maxWidth="150"
+          maxWidth="lg"
+          xs={12} sm={12} md={6}
         >
-          <DialogTitle variant="title" id="scroll-dialog-title" align="center">{eventName}</DialogTitle>
-          <DialogContent>
-            <Typography color="inherit" paragraph align="center" variant="body1">
-              {description}
-            </Typography>
-
-            <Grid container spacing={12}>
-              <Typography variant="h5" color="inherit" paragraph>
-                            Event Date:
-              </Typography>
-              <Grid container spacing={12}>
-                <Grid item xs={12}>
-                                Start date:
-                  {' '}
-                  {dateStart.substring(0, 10)}
-                </Grid>
-                <Grid item xs={12}>
-                                End date:
-                  {' '}
-                  {dateEnd.substring(0, 10)}
-                </Grid>
-              </Grid>
-            </Grid>
-
-            <Typography variant="h5" color="inherit" paragraph>
-              <br />
-Event Time:
-            </Typography>
-
-            <Grid justify="center" container item xs>
-              <Grid container spacing={12}>
-                <Grid item xs={6}>
-                                Start time:
-                  {' '}
-                  {timeStart}
-                </Grid>
-                <Grid item xs={6}>
-                                End time:
-                  {' '}
-                  {timeEnd}
-                </Grid>
-              </Grid>
-            </Grid>
-
-            {location !== undefined && (
-            <Grid container spacing={12}>
-              <Typography variant="h5" color="inherit" paragraph>
-                <br />
-Location:
-              </Typography>
-              <Grid container spacing={12}>
-                <Grid item xs={12}>
-                                    Address:
-                  {' '}
-                  {location.address}
-                </Grid>
-                <Grid item xs={12}>
-                                    Apartment:
-                  {' '}
-                  {location.apartment}
-                </Grid>
-                <Grid item xs={12}>
-                                    City:
-                  {' '}
-                  {location.city}
-                </Grid>
-                <Grid item xs={12}>
-                                    Province:
-                  {' '}
-                  {location.province}
-                </Grid>
-                <Grid item xs={12}>
-                                    Postal Code:
-                  {' '}
-                  {location.postalCode}
-                </Grid>
-                <Grid item xs={12}>
-                                    Phone Number:
-                  {' '}
-                  {location.phoneNumber}
-                </Grid>
-              </Grid>
-            </Grid>
-            )}
-            {open && location !== undefined && (
-            <GoogleMaps
-              location={location}
-            />
-            )}
-          </DialogContent>
-          <DialogActions> 
-            { type === UserTypes.ADMIN
-               && (
-                 <Button onClick={this.handleDelete} color="secondary">
-                   Delete
-                 </Button>
-               )
-            }}
-            {this.props.editMode &&
-              <React.Fragment>
-                  <Button onClick={this.handleDelete} color="secondary">
-                      Delete
+          <GridItem>
+            <Card>
+              <CardHeader>
+                <h4 className={classes.cardTitle}>
+                  {eventName} <small> {dateStart.substring(0, 10)}</small>
+                </h4>
+              </CardHeader>
+              <CardBody>
+                <NavPills
+                  color="rose"
+                  horizontal={{
+                    tabsGrid: { xs: 12, sm: 12, md: 4 },
+                    contentGrid: { xs: 12, sm: 12, md: 8 }
+                  }}
+                  tabs={[
+                    {
+                      tabButton: "Description",
+                      tabIcon: Dashboard,
+                      tabContent: (
+                        <span>
+                          <p>
+                            <center>{description}</center>
+                          </p>
+                          <br />
+                          <p>
+                          <b>Start date:</b>
+                          {' '}
+                          {dateStart.substring(0, 10)}
+                          {' '}
+                          <b>End date:</b>
+                          {' '}
+                          {dateEnd.substring(0, 10)}
+                          {' '}
+                          <b>Start time:</b>
+                          {' '}
+                          {timeStart}
+                          {' '}
+                          <b>End time:</b>
+                          {' '}
+                          {timeEnd}
+                          </p>
+                          <p>
+                            {location !== undefined && (
+                              <p>
+                                  <center><h6><b>Location</b></h6></center>
+                                  <Grid>
+                                    <b>Address:</b>
+                                    {' '}
+                                    {location.address}
+                                    <br />
+                                    <b>Apartment:</b>
+                                    {' '}
+                                    {location.apartment}
+                                    <br />
+                                    <b>City:</b>
+                                    {' '}
+                                    {location.city}
+                                    <br />
+                                    <b>Province:</b>
+                                    {' '}
+                                    {location.province}
+                                    <br />
+                                    <b>Postal Code:</b>
+                                    {' '}
+                                    {location.postalCode}
+                                    <br />
+                                    <b>Phone Number:</b>
+                                    {' '}
+                                    {location.phoneNumber}
+                                    <br />
+                                    </Grid>
+                                    </p>
+                              )}
+                          </p>
+                        </span>
+                      )
+                    },
+                    {
+                      tabButton: "Schedule",
+                      tabIcon: Schedule,
+                      tabContent: (
+                        <span>
+                            <CardHeader color="rose" icon>
+                              <CardIcon color="rose">
+                                <AddLocation />
+                              </CardIcon>
+                              <h4 className={classes.cardIconTitle}>Map</h4>
+                            </CardHeader>
+                            <CardBody>
+                              <RegularMap
+                                googleMapURL="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"
+                                loadingElement={<div style={{ height: `100%` }} />}
+                                containerElement={
+                                  <div
+                                    style={{
+                                      height: `280px`,
+                                      borderRadius: "6px",
+                                      overflow: "hidden"
+                                    }}
+                                  />
+                                }
+                                mapElement={<div style={{ height: `100%` }} />}
+                              />
+                            </CardBody>
+                        </span>
+                      )
+                    }
+                  ]}
+                />
+              </CardBody>
+                <DialogActions> 
+                  { type === UserTypes.ADMIN
+                    && (
+                      <Button onClick={this.handleDelete} color="secondary">
+                        Delete
+                      </Button>
+                    )
+                  }}
+                  {this.props.editMode &&
+                    <React.Fragment>
+                        <Button onClick={this.handleDelete} color="secondary">
+                            Delete
+                        </Button>
+                        <Button onClick={this.handleEdit} color="primary">
+                            Edit
+                        </Button>
+                    </React.Fragment>
+                    }
+                  <Button onClick={onClose} color="primary">
+                    Cancel
                   </Button>
-                  <Button onClick={this.handleEdit} color="primary">
-                      Edit
-                  </Button>
-              </React.Fragment>
-              }
-            <Button onClick={onClose} color="primary">
-              Cancel
-            </Button>
-          </DialogActions>
+                </DialogActions>
+            </Card>
+          </GridItem>
         </Dialog>
+        
       </div>
     );
   }
