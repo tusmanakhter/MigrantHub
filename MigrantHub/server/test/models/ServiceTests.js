317bf4e2600c939985385906b56481478b4e0410
Alexandre Masmoudi <a-masmoudi@hotmail.com>
2019-01-21 11:35:49 -0500

    Fixed test

--- MigrantHub/server/test/models/ServiceTests.js
+++ MigrantHub/server/test/models/ServiceTests.js
@@ -3,14 +3,12 @@ var expect = chai.expect;
 var Service = require('../../models/Service');
 
 describe('Services model', function() {
-    it('Should be invalid if user, serviceTitle, serviceDescription or serviceSummary is empty', function(done) {
+    it('Should be invalid if user or serviceTitle is empty', function(done) {
         var service = new Service();
 
         service.validate(function(err) {
             expect(err.errors.user).to.exist;
             expect(err.errors.serviceTitle).to.exist;
-            expect(err.errors.serviceDescription).to.exist;
-            expect(err.errors.serviceSummary).to.exist;
             done();
         });
     });
