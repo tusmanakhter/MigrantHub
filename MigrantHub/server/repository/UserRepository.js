6b734a2334c1c38e341cad47cc55457f165de242
Laxman <laxman_20@hotmail.com>
2019-01-25 18:06:22 -0500

    #145 Added server code that will send a verification code to the user and reset the password if the verification code is correct and theirs. It emails the user a token that is valid for 1hr. Also added a userType field to the model, so that it can be used to specify if a user is a local, google or facebook user. This is needed since only local users can change their password.

--- MigrantHub/server/repository/UserRepository.js
+++ MigrantHub/server/repository/UserRepository.js
@@ -9,4 +9,11 @@ module.exports = {
         throw new ServerError('There was an error retrieving user.', 400, error);
       });
   },
+
+  update(userId, userObject) {
+      return User.findByIdAndUpdate({ _id: userId }, userObject, { new: true })
+          .exec().then(() => Promise.resolve('User has been updated.')).catch((error) => {
+          throw new ServerError('There was an error updating user.', 400, error);
+      });
+  },
 };
