ba6f657904bc148a23cf03f1fd6f4f07a30e6f7c
Mira <miramarhaba@hotmail.com>
2019-01-31 01:10:38 -0500

    #132 - adding the skeleton of the api for the ML recommender microservice

---
+++ MigrantHub/data_models/Service_Recommender/Service_Recommender/Controllers/RecommendationController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Service_Recommender.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RecommendationController : ControllerBase
+    {
+        // GET api/recommendation/{id}
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+
+
+            return id.ToString();
+        }
+    }
+}
