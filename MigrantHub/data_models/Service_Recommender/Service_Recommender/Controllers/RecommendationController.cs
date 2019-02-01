cfe097d3c88a72cea87a8ac1af8176efd92afbff
Mira <miramarhaba@hotmail.com>
2019-02-01 00:32:37 -0500

    #132 final variable fixes and cleanup

--- MigrantHub/data_models/Service_Recommender/Service_Recommender/Controllers/RecommendationController.cs
+++ MigrantHub/data_models/Service_Recommender/Service_Recommender/Controllers/RecommendationController.cs
@@ -1,50 +1,16 @@
 ﻿using System;
 using System.IO;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using System.Net.Http;
-using System.Text;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
-using Newtonsoft.Json;
 using Microsoft.ML;
 using Microsoft.ML.Runtime.Api;
 using Microsoft.ML.Runtime.Data;
-using Microsoft.ML.Runtime.Learners;
-using Microsoft.ML.Runtime;
 using Microsoft.ML.Core.Data;
-using Microsoft.ML.Runtime.Training;
-using Microsoft.ML.StaticPipe;
-using Microsoft.ML.Trainers;
-using Google.Apis.Storage.v1;
 using Google.Cloud.Storage.V1;
 
 namespace Service_Recommender.Controllers
 {
-    public class RatingData
-    {
-        [Column("0")]
-        public string userId;
-
-        [Column("1")]
-        public string serviceId;
-
-        [Column("2")]
-        [ColumnName("Label")]
-        public float Label;
-    }
-
-    public class RatingPrediction
-    {
-        [ColumnName("PredictedLabel")]
-        public bool predictedLabel;
-
-        public float Score;
-    }
-
     [Route("api/[controller]")]
     [ApiController]
     public class RecommendationController : ControllerBase
@@ -72,10 +38,9 @@ namespace Service_Recommender.Controllers
                 storage.DownloadObject(bucketPath, "allServices.csv", outputFile);
             }
 
-            // create the local environment
+            // create the local environment and load the ServicesRecommendation Model
             var ctx = new MLContext();
 
-            //2. load the ServicesRecommendation Model
             ITransformer loadedModel;
             using (var stream = new FileStream(modelPath, FileMode.Open, FileAccess.Read, FileShare.Read))
             {
@@ -88,41 +53,51 @@ namespace Service_Recommender.Controllers
             //create a prediction function
             var predictionfunction = loadedModel.MakePredictionFunction<RatingData, RatingPrediction>(ctx);
 
-            List<Tuple<int, float>> ratings = new List<Tuple<int, float>>();
-            //List<Tuple<int, int>> ServiceRatings = _profileService.GetProfileWatchedServices(id);
-            //List<Service> WatchedServices = new List<Service>();
+            // find the rating prediction for each service and compile the normalized predictions into a list
+            List<Tuple<int, float>> predictions = new List<Tuple<int, float>>();
 
-            //foreach (Tuple<int, int> tuple in ServiceRatings)
-            //{
-            //    WatchedServices.Add(_serviceService.Get(tuple.Item1));
-            //}
-
-            // find the rating prediction for each service
             RatingPrediction prediction = null;
-            //foreach (var service in serviceIds)
             for (var i = 0; i < serviceIds.Length; i++)
             {
-                var serviceLine = serviceIds[i];
-                var serviceLineSplit = serviceLine.Split(',');
-                var serviceId = serviceLineSplit[0];
-                
-                //4. Call the Rating Prediction for each service prediction
+                var serviceLine = serviceIds[i].Split(',');
+                var serviceId = serviceLine[0];
+
                 prediction = predictionfunction.Predict(new RatingData { userId = id.ToString(), serviceId = serviceId });
 
-                //5. Normalize the prediction scores for the "ratings" b/w 0 - 100
                 var normalizedscore = Sigmoid(prediction.Score);
 
-                //6. Add the score for recommendation of each service in the trending service list
-                ratings.Add(Tuple.Create(Int32.Parse(serviceId), normalizedscore));
+                predictions.Add(Tuple.Create(Int32.Parse(serviceId), normalizedscore));
             }
 
-            //Get the top 3 predictions
-            ratings.Sort((x, y) => y.Item2.CompareTo(x.Item2));
-            if (ratings.Count > 3) ratings.RemoveRange(3, ratings.Count - 3);
+            //get the top 3 predictions
+            predictions.Sort((x, y) => y.Item2.CompareTo(x.Item2));
+            if (predictions.Count > 3) predictions.RemoveRange(3, predictions.Count - 3);
 
-            //return id.ToString();
-            var output = string.Join(",", ratings.Select(t => string.Format("[ '{0}', '{1}']", t.Item1, t.Item2)));
-            return output;
+            //return the predictions in string format
+            var recommendations = string.Join(",", predictions.Select(t => string.Format("[ '{0}', '{1}']", t.Item1, t.Item2)));
+
+            return recommendations;
+        }
+
+        public class RatingData
+        {
+            [Column("0")]
+            public string userId;
+
+            [Column("1")]
+            public string serviceId;
+
+            [Column("2")]
+            [ColumnName("Label")]
+            public float Label;
+        }
+
+        public class RatingPrediction
+        {
+            [ColumnName("PredictedLabel")]
+            public bool predictedLabel;
+
+            public float Score;
         }
 
         public float Sigmoid(float x)
