8df9c832d86a20e14d0b5517b322dea322a045c9
Alexandre Masmoudi <a-masmoudi@hotmail.com>
2019-02-05 23:35:44 -0500

    Fixed file uploading for ML on cloud

--- MigrantHub/server/db/generateMLData.js
+++ MigrantHub/server/db/generateMLData.js
@@ -5,20 +5,20 @@ const fs = require('fs');
 var MongoClient = require('mongodb').MongoClient;
 const { dbConnectionString } = require('../config');
 
-const url = dbConnectionString();
+const url = dbConnectionString(); 
 
 this.generateServiceRecommendationData = function generateServiceReviewsData() {
 
   // FILE CREATION
   // File of all services
-  fs.writeFile("../data_models/Service_Rating_Model/Service_Recommender/Data/allServices.csv", "", function(err){
+  fs.writeFile("../data_models/Service_Rating_Model/Service_Recommender/allServices.csv", "", function(err){
     if(err) {
         console.log(err);
     }
   });
 
   // File of service reviews
-  fs.writeFile("../data_models/Service_Rating_Model/Service_Recommender/Data/ratings.csv", "", function(err){
+  fs.writeFile("../data_models/Service_Rating_Model/Service_Recommender/ratings.csv", "", function(err){
     if(err) {
         console.log(err);
     }
@@ -32,14 +32,14 @@ this.generateServiceRecommendationData = function generateServiceReviewsData() {
     dbo.collection("services").find({}).forEach(function(doc) {
       var text = doc._id + ';' + doc.serviceTitle;
       if(!doc.deleted) {
-        fs.appendFile("../data_models/Service_Rating_Model/Service_Recommender/Data/allServices.csv", [text + '\n'], function(err) {
+        fs.appendFile("../data_models/Service_Rating_Model/Service_Recommender/allServices.csv", [text + '\n'], function(err) {
           if(err) {
               console.log(err);
           }
         })
       }
     }, function(err) {
-      uploadFile('data_model_files', '../data_models/Service_Rating_Model/Service_Recommender/Data/allServices.csv');
+      uploadFile('data_model_files', '../data_models/Service_Rating_Model/Service_Recommender/allServices.csv');
       if(err) {
         console.log(err);
       }
@@ -53,14 +53,14 @@ this.generateServiceRecommendationData = function generateServiceReviewsData() {
     dbo.collection("reviews").find({}).forEach(function(doc) {
       var text = doc.user + ';' + doc.serviceId + ';' + doc.rating + '; 0000';
       if(!doc.deleted) {
-        fs.appendFile("../data_models/Service_Rating_Model/Service_Recommender/Data/ratings.csv", ['\n' + text], function(err) {
+        fs.appendFile("../data_models/Service_Rating_Model/Service_Recommender/ratings.csv", ['\n' + text], function(err) {
           if(err) {
               console.log(err);
           }
         })
       }
     }, function(err) {
-      uploadFile('data_model_files', '../data_models/Service_Rating_Model/Service_Recommender/Data/ratings.csv');
+      uploadFile('data_model_files', '../data_models/Service_Rating_Model/Service_Recommender/ratings.csv');
       if(err) {
         console.log(err);
       }
@@ -72,14 +72,13 @@ this.generateServiceRecommendationData = function generateServiceReviewsData() {
 // COMMENTED OUT uploadFile() SO IT DOESNT RUN LOCALLY! WILL BE UNCOMMENTED ONCE IN THE CLOUD
 
 // Upload to Google Storage Bucket
-if(process.env.MONGO_ATLAS) {
-  async function uploadFile(bucketName, filename) {
+async function uploadFile(bucketName, filename) {
+  if(process.env.MONGO_ATLAS) {
     // Imports the Google Cloud client library
     const {Storage} = require('@google-cloud/storage');
   
     // Creates a client
     const storage = new Storage();
-  
     // Uploads a local file to the bucket
     await storage.bucket(bucketName).upload(filename, {
       // Support for HTTP requests made with `Accept-Encoding: gzip`
@@ -90,4 +89,5 @@ if(process.env.MONGO_ATLAS) {
     });
   }
 }
+
 
