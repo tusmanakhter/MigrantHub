180446ca6c1a0385586a175a8180c8642f0ed8b0
tusmanakhter <tusman96@hotmail.com>
2019-03-08 18:27:46 -0500

    Remove home header and footer files
    Added their code to home layout file
    They are only used in one place and add complexity

--- MigrantHub/client/src/home/HomeLayout.js
+++ MigrantHub/client/src/home/HomeLayout.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import HomeHeader from 'home/HomeHeader';
-import HomeFooter from 'home/HomeFooter';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { Link } from 'react-router-dom';
+import { FormattedMessage } from 'react-intl';
+import { FaGithub, FaRegCopyright } from 'react-icons/fa';
+import LanguageSwitch from 'components/LanguageSwitch';
 
-const styles = {
+const styles = theme => ({
   content: {
     flex: '1 0 auto',
     display: 'flex',
@@ -19,7 +24,95 @@ const styles = {
     display: 'flex',
     flexDirection: 'column',
   },
-};
+  header: {
+    '& a': {
+      [theme.breakpoints.down('sm')]: {
+        marginRight: 10,
+      },
+      color: 'white',
+      marginRight: 20,
+    },
+    '& a:hover': {
+      color: '#e5e5e5',
+    },
+  },
+  title: {
+    display: 'block',
+    marginRight: 'auto',
+    [theme.breakpoints.down('xs')]: {
+      marginRight: 'initial',
+    },
+  },
+  appBar: {
+    padding: '0% 7.5%',
+    background: theme.palette.primary.main,
+  },
+  toolbar: {
+    [theme.breakpoints.down('xs')]: {
+      padding: '0% 1% 0% 2%',
+      display: 'flex',
+      flexDirection: 'column',
+      textAlign: 'center',
+    },
+  },
+  underline: {
+    background: 'white',
+    content: '""',
+    display: 'block',
+    height: '2px',
+    opacity: '.5',
+    position: 'absolute',
+    width: '100%',
+  },
+  root: {
+    display: 'flex',
+    alignItems: 'center',
+    background: theme.palette.primary.main,
+    color: 'white',
+    flexGrow: 1,
+    padding: '10px 20px 10px 20px',
+    '& a': {
+      color: 'white',
+      marginRight: 20,
+    },
+    '& a:hover': {
+      color: '#e5e5e5',
+    },
+    '& h6': {
+      marginRight: 15,
+    },
+    flexWrap: 'wrap',
+    [theme.breakpoints.down('sm')]: {
+      justifyContent: 'center',
+    },
+  },
+  right: {
+    marginLeft: 'auto',
+    display: 'flex',
+    textAlign: 'center',
+    alignItems: 'center',
+    [theme.breakpoints.down('sm')]: {
+      justifyContent: 'center',
+      marginLeft: 'initial',
+      textAlign: 'center',
+    },
+  },
+  languageSwitcher: {
+    marginRight: 15,
+  },
+  copyright: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+  iconText: {
+    margin: 'auto',
+    marginLeft: 5,
+  },
+  center: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+});
 
 const HomeLayout = (props) => {
   const { classes, children } = props;
@@ -28,11 +121,49 @@ const HomeLayout = (props) => {
     <React.Fragment>
       <div className={classes.layout}>
         <div className={classes.content}>
-          <HomeHeader />
+          <div className={classes.header}>
+            <AppBar position="static" className={classes.appBar}>
+              <Toolbar className={classes.toolbar}>
+                <Typography variant="h6" color="inherit" className={classes.title}>
+                  <Link to="/">MigrantHub</Link>
+                </Typography>
+                <Link to="/login">
+                  <FormattedMessage id="login" />
+                </Link>
+                <Link to="/signup/account-selection">
+                  <FormattedMessage id="signup" />
+                </Link>
+              </Toolbar>
+            </AppBar>
+            <div className={classes.underline} />
+          </div>
           { children }
         </div>
         <div className={classes.footer}>
-          <HomeFooter />
+          <div className={classes.root}>
+            <Typography variant="subtitle2" color="inherit" component={Link} to="/">
+              <FormattedMessage id="home" />
+            </Typography>
+            <Typography variant="subtitle2" color="inherit">
+              <FormattedMessage id="footer.privacypolicy" />
+            </Typography>
+            <Typography variant="subtitle2" color="inherit">
+              <FormattedMessage id="footer.faq" />
+            </Typography>
+            <Typography variant="subtitle2" color="inherit">
+              <FormattedMessage id="contact" />
+            </Typography>
+            <Typography variant="subtitle2" color="inherit" className={classes.copyright}>
+              <FaRegCopyright />
+              <p className={classes.iconText}>2019 MigrantHub</p>
+            </Typography>
+            <div className={classes.right}>
+              <div className={classes.languageSwitcher}>
+                <LanguageSwitch dark />
+              </div>
+              <a href="https://github.com/tusmanakhter/MigrantHub" target="_blank" rel="noopener noreferrer" className={classes.center}><FaGithub size="2em" /></a>
+            </div>
+          </div>
         </div>
       </div>
     </React.Fragment>
