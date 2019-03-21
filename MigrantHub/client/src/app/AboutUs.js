dd0b83a49dec1218bad8bff98a8131986dfc3640
CodeTaj <tajbidhussain@hotmail.com>
2019-03-20 20:26:41 -0400

    Changed design to make it more compact, added more information. #229

--- MigrantHub/client/src/app/AboutUs.js
+++ MigrantHub/client/src/app/AboutUs.js
@@ -1,11 +1,8 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-import { Link } from 'react-router-dom';
-import { FormattedMessage, FormattedHTMLMessage } from 'react-intl';
+import RefugeeCenterLogo from 'assets/img/refugee_centre_logo.png';
 
 const styles = theme => ({
   root: {
@@ -21,88 +18,106 @@ const styles = theme => ({
       padding: '3%',
     },
     textSize: '10px',
-    textAlign: 'center',
     color: 'white',
-    padding: '10% 25% 10% 25%',
     background: theme.palette.primary.main,
+    padding: '1% 15% 1% 15%',
+    display: 'inline-block',
   },
   aims: {
     [theme.breakpoints.down('sm')]: {
       padding: '3%',
     },
+    padding: '1% 15% 1% 15%',
     background: theme.palette.common.white,
     color: 'black',
-    padding: '3% 25% 3% 25%',
-  },
-  about: {
-    [theme.breakpoints.down('sm')]: {
-      padding: '3%',
-    },
-    color: 'black',
-    padding: '3% 20% 3% 20%',
-    background: theme.palette.secondary.light,
-  },
-  content: {
-    marginTop: '5px',
-    '& a': {
-      color: 'inherit',
-    },
-    '& a:hover': {
-      color: 'inherit',
-      textDecoration: 'underline',
-    },
   },
 });
 
-const HomePage = (props) => {
-  const { classes } = props;
+class AboutUs extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-  return (
-    <div className={classes.root}>
-      <Grid container>
-        <Grid item xs={12} className={classes.summary}>
-        <h1>About Us</h1>
-        <h5>We are a group of software engineering students working to create the most innovatif service platform for newcomers in Montreal.
-        This project is powered by <b>The Refugee Centre</b>.</h5>
-        <hr />
-        <h3>Aims</h3>
-        <ls>
-          <li>Facilitate integration for newcomers</li>
-          <li>Promote diversity and inclusion</li>
-          <li>Help businesses reach out to newcomers</li>
-          <li>Learn about newcomers using the website and guide them using AI technology</li>
-        </ls>
-        <h3>Objectives</h3>
-        <ls>
-          <li>Provide certified services to all users for free</li>
-          <li>Learn about our users to personalize the types of services they might like</li>
-          <li>Assist newcomers in what they are looking for </li>
-          <li>Centralize all available services in Montreal into one place</li>
-        </ls>
-        <hr />
-        <h3>Contact MigrantHub</h3>
-        <ls>
-          <p>Have questions regarding the website? OR Want to help the project? Please feel free to send us an email at:</p>
-          <h4>mtl-migranthub@placeholder.com</h4>
-        </ls>
-        </Grid>
-        <Grid item xs={12} className={classes.aims}>
-        <h3>Current Members</h3>
-        <a href='https://github.com/tusmanakhter'><h5>Tusman Akther</h5></a>
-        <a href='https://github.com/CodeTaj'><h5>Tajbid Choudhury</h5></a>
-        <a href='https://github.com/miramarhaba'><h5>Mira Marhaba</h5></a> 
-        <a href='https://github.com/Smoudii'><h5>Alexandre Masmoudi</h5></a>
-        <a href='https://github.com/mazennahle'><h5>Mazen Nahle</h5></a>
-        <a href='https://github.com/rajee10'><h5>Rajee Vairamuthu</h5></a>
-        <a href='https://github.com/iamlax'><h5>Laxman Velauthapillai</h5></a>
-        </Grid>
-      </Grid>
-    </div>
-  );
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <React.Fragment>
+        <div className={classes.root}>
+          <Grid container>
+            <Grid item xs={12} className={classes.summary}>
+              <h1>About Us</h1>
+              <h5>
+                We are a group of software engineering students working to create the most innovative service platform for newcomers in Montreal. 
+                MigrantHub was started because of refugees, immigrants, international students that have a hard time finding services that they need. 
+                Many times we find ourselves not even knowing the existence of a particular service. 
+                This is where MigrantHub comes in, bringing all services available in Montreal into one simple website. 
+              </h5>
+              This project is powered by <b>The Refugee Centre</b>.
+              <hr />
+              <div style={{ float: 'center', display: 'inline-block' }}>
+                <h3>Aims</h3>
+                <ls>
+                  <p>Facilitate integration for newcomers</p>
+                  <p>Promote diversity and inclusion</p>
+                  <p>Help businesses reach out to newcomers</p>
+                  <p>Learn about newcomers using the website and guide them using AI technology</p>
+                </ls>
+                <br />
+              </div>
+              <div style={{ float: 'center', display: 'inline-block' }}>
+                <h3>Objectives</h3>
+                <ls>
+                  <p>Provide certified services to all users for free</p>
+                  <p>Learn about our users to personalize the types of services they might like</p>
+                  <p>Assist newcomers in what they are looking for </p>
+                  <p>Centralize all available services in Montreal into one place</p>
+                </ls>
+                <br />
+              </div>
+              <div style={{ clear: 'both' }}>
+                <hr />
+                <h3>Contact MigrantHub</h3>
+                <ls>
+                  <p>Have questions regarding the website? OR Want to help the project? Please feel free to send us an email at:</p>
+                  <h4>mtl-migranthub@placeholder.com</h4>
+                </ls>
+              </div>
+              <br />
+            </Grid>
+            <Grid item xs={12} className={classes.aims}>
+              <h3>Current Members</h3>
+              <div style={{ float: 'center', display: 'inline-block' }}>
+                <a href="https://github.com/tusmanakhter">Tusman Akther</a><br />
+                <a href="https://github.com/CodeTaj">Tajbid Choudhury</a><br />
+                <a href="https://github.com/miramarhaba">Mira Marhaba</a><br />
+                <a href="https://github.com/Smoudii">Alexandre Masmoudi</a><br />
+                <a href="https://github.com/mazennahle">Mazen Nahle</a><br />
+                <a href="https://github.com/rajee10">Rajee Vairamuthu</a><br />
+                <a href="https://github.com/iamlax">Laxman Velauthapillai</a><br />
+              </div>
+              <hr />
+              <div style={{ float: 'center', display: 'inline-block' }}>
+                <a href="https://www.therefugeecentre.org/">
+                  <img src={RefugeeCenterLogo} alt="..." height="25%" width="25%" />
+                </a>
+                <br />
+              </div>
+            </Grid>
+          </Grid>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+AboutUs.defaultProps = {
+  classes: {},
 };
 
-HomePage.propTypes = {
+AboutUs.propTypes = {
   classes: PropTypes.object.isRequired,
 };
 
-export default withStyles(styles)(HomePage);
+export default withStyles(styles)(AboutUs);
