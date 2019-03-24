a26f20a4528fa85c2f391e5969b2eb9fadeb9b27
Mazen Nahle <nahlemazen@gmail.com>
2019-03-24 00:26:21 -0400

    Added back end logics for pinned services

---
+++ MigrantHub/server/models/PinnedService.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const { Schema } = require('mongoose');
+
+const options = { discriminatorKey: 'type' };
+
+const PinnedService = new Schema({
+    _id: { type: String, required: true },
+    pinnedList: [{
+      serviceId: { type: String, default: '' },
+      deleted: { type: Boolean, default: false },
+    }],
+  }, options);
+
+module.exports = mongoose.model('PinnedService', PinnedService);
