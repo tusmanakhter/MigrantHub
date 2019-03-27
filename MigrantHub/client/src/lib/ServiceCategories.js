6ba029bfffe39071314547e3197521c40f436d88
CodeTaj <tajbidhussain@hotmail.com>
2019-03-26 20:46:41 -0400

    Added new categories listing design with proper rerouting #227

--- MigrantHub/client/src/lib/ServiceCategories.js
+++ MigrantHub/client/src/lib/ServiceCategories.js
@@ -1,68 +1,91 @@
 export const serviceCategories = [
-    {  value:  'WorkAccidents' , label: 'Work Accidents',
-        SubCategories: [
-            {  value:  'CommunityOrganizations' , label: 'Community Organizations' }
-        ]},
-    {  value:  'AutomobileAccidents' , label: 'Accidents' },
-    {  value:  'Welfare' , label: 'Welfare' },
-    {  value:  'CommunityOrganizations' , label: 'Community Organizations' },
-    {  value:  'BudgetAndDebtProblems' , label: 'Budget And Debt Problems' },
-    {  value:  'ConsumerProtection' , label: 'Consumer Protection' },
-    {  value:  'CommunityGroups' , label: 'Community Groups' },
-    {  value:  'EmergencyAid' , label: 'Emergency Aid',
-        SubCategories: [
-            {  value:  'Food' , label: 'Food' }
-        ]},
-    {  value:  'SecondHandClothing' , label: 'Second-Hand Clothing' },
-    {  value:  'SecondHandFurnitureAndClothing' , label: 'Second-Hand Furniture And Clothing' },
-    {  value:  'Employment' , label: 'Employment',
-        SubCategories: [
-            {  value:  'CommunityOrganizations' , label: 'Community Organizations' },
-            {  value:  'SmallBusiness' , label: 'Small Business' },
-            {  value:  'JobSearch' , label: 'Job Search' },
-        ]},
-    {  value:  'EmploymentInsurance' , label: 'Employment Insurance' },
-    {  value:  'CommunityOrganizations' , label: 'Community Organizations' },
-    {  value:  'FamilyBenefits' , label: 'Family Benefits' },
-    {  value:  'Women' , label: 'Women' },
-    {  value:  'Immigration' , label: 'Immigration' },
-    {  value:  'Passport' , label: 'Passport' },
-    {  value:  'IncomeTaxes' , label: 'Income Taxes' },
-    {  value:  'ResourcesForLGBTQPeople' , label: 'Resources For Lgbtq+ People' },
-    {  value:  'Housing' , label: 'Housing' },
-    {  value:  'ApartmentSearch' , label: 'Apartment Search' },
-    {  value:  'InfoForLandlords' , label: 'Info For Landlords' },
-    {  value:  'SocialHousing' , label: 'Social Housing' },
-    {  value:  'CDNCommunityOrganizations' , label: 'C-D-N Community Organizations' },
-    {  value:  'Pensions' , label: 'Pensions' },
-    {  value:  'Seniors' , label: 'Seniors',
-        SubCategories: [
-            {  value:  'CommunityOrganizations' , label: 'Community Organizations' },
-            {  value:  'SeniorsVolunteerSupportServices' , label: 'Seniors’ Volunteer Support Services' },
-            {  value:  'SeniorsMealDelivery' , label: 'Seniors’ Meal Delivery' },
-            {  value:  'SeniorsMiscellaneous' , label: 'Seniors’ Miscellaneous' }
-        ]},
-    {  value:  'DisabledPeople' , label: 'Disabled People',
-        SubCategories: [
-            {  value:  'CommunityOrganizations' , label: 'Community Organizations' },
-        ]},
-    {  value:  'MentalHealth' , label: 'Mental Health',
-        SubCategories: [
-            {  value:  'ListeningServices' , label: 'Listening Services' },
-            {  value:  'CrisisCentres' , label: 'Crisis Centres' }
-        ]},
-    {  value:  'HealthAndSocialServices' , label: 'Health And Social Services',
-        SubCategories: [
-            {  value:  'CommunityOrganizations' , label: 'Community Organizations' },
-            {  value:  'Hospitals' , label: 'Hospitals' }
-        ]},
-    {  value:  'InformationAndReferralServices' , label: 'Information & Referral Services' },
-    {  value:  'LegalAid' , label: 'Legal Aid',
-        SubCategories: [
-            {  value:  'CommunityLegalClinics' , label: 'Community Legal Clinics' },
-            {  value:  'Courts' , label: 'Courts' }
-        ]},
-    {  value:  'MunicipalServices' , label: 'Municipal Services' },
-    {  value:  'VictimsOfCrime' , label: 'Victims Of Crime' },
+  { value: 'ApartmentSearch', label: 'Apartment Search' },
+  { value: 'AutomobileAccidents', label: 'Accidents' },
+  { value: 'BudgetAndDebtProblems', label: 'Budget And Debt Problems' },
+  { value: 'CDNCommunityOrganizations', label: 'C-D-N Community Organizations' },
+  { value: 'CommunityGroups', label: 'Community Groups' },
+  { value: 'CommunityOrganizations', label: 'Community Organizations' },
+  { value: 'CommunityOrganizations', label: 'Community Organizations' },
+  { value: 'ConsumerProtection', label: 'Consumer Protection' },
+  {
+    value: 'DisabledPeople',
+    label: 'Disabled People',
+    SubCategories: [
+      { value: 'CommunityOrganizations', label: 'Community Organizations' },
+    ],
+  },
+  {
+    value: 'EmergencyAid',
+    label: 'Emergency Aid',
+    SubCategories: [
+      { value: 'Food', label: 'Food' },
+    ],
+  },
+  {
+    value: 'Employment',
+    label: 'Employment',
+    SubCategories: [
+      { value: 'CommunityOrganizations', label: 'Community Organizations' },
+      { value: 'JobSearch', label: 'Job Search' },
+      { value: 'SmallBusiness', label: 'Small Business' },
+    ],
+  },
+  { value: 'EmploymentInsurance', label: 'Employment Insurance' },
+  { value: 'FamilyBenefits', label: 'Family Benefits' },
+  {
+    value: 'HealthAndSocialServices',
+    label: 'Health And Social Services',
+    SubCategories: [
+      { value: 'CommunityOrganizations', label: 'Community Organizations' },
+      { value: 'Hospitals', label: 'Hospitals' },
+    ],
+  },
+  { value: 'Housing', label: 'Housing' },
+  { value: 'Immigration', label: 'Immigration' },
+  { value: 'IncomeTaxes', label: 'Income Taxes' },
+  { value: 'InfoForLandlords', label: 'Info For Landlords' },
+  { value: 'InformationAndReferralServices', label: 'Information & Referral Services' },
+  {
+    value: 'LegalAid',
+    label: 'Legal Aid',
+    SubCategories: [
+      { value: 'CommunityLegalClinics', label: 'Community Legal Clinics' },
+      { value: 'Courts', label: 'Courts' },
+    ],
+  },
+  {
+    value: 'MentalHealth',
+    label: 'Mental Health',
+    SubCategories: [
+      { value: 'CrisisCentres', label: 'Crisis Centres' },
+      { value: 'ListeningServices', label: 'Listening Services' },
+    ],
+  },
+  { value: 'MunicipalServices', label: 'Municipal Services' },
+  { value: 'Passport', label: 'Passport' },
+  { value: 'Pensions', label: 'Pensions' },
+  { value: 'ResourcesForLGBTQPeople', label: 'Resources For Lgbtq+ People' },
+  { value: 'SecondHandClothing', label: 'Second-Hand Clothing' },
+  { value: 'SecondHandFurnitureAndClothing', label: 'Second-Hand Furniture And Clothing' },
+  {
+    value: 'Seniors',
+    label: 'Seniors',
+    SubCategories: [
+      { value: 'CommunityOrganizations', label: 'Community Organizations' },
+      { value: 'SeniorsVolunteerSupportServices', label: 'Seniors’ Volunteer Support Services' },
+      { value: 'SeniorsMealDelivery', label: 'Seniors’ Meal Delivery' },
+      { value: 'SeniorsMiscellaneous', label: 'Seniors’ Miscellaneous' },
+    ],
+  },
+  { value: 'SocialHousing', label: 'Social Housing' },
+  { value: 'VictimsOfCrime', label: 'Victims Of Crime' },
+  { value: 'Welfare', label: 'Welfare' },
+  { value: 'Women', label: 'Women' },
+  {
+    value: 'WorkAccidents',
+    label: 'Work Accidents',
+    SubCategories: [
+      { value: 'CommunityOrganizations', label: 'Community Organizations' },
+    ],
+  },
 ];
-
