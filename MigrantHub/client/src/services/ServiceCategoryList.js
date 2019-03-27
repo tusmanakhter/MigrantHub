b2fe9e288c5d31715377b5565849463f3f5adb6f
CodeTaj <tajbidhussain@hotmail.com>
2019-03-26 20:46:41 -0400

    Added new categories listing design with proper rerouting #227

--- MigrantHub/client/src/services/ServiceCategoryList.js
+++ MigrantHub/client/src/services/ServiceCategoryList.js
@@ -1,22 +1,101 @@
 import React, { Component } from 'react';
-import ServiceCategories from 'services/ServiceCategoryMenu2';
 import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import { Redirect } from 'react-router-dom';
+import { serviceCategories } from 'lib/ServiceCategories';
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Typography from "@material-ui/core/Typography";
+const styles = {
+  card: {
+    width: 150,
+    height: 120,
+  },
+};
 
 class ServiceCategoryList extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      redirectTo: false,
+      redirectToURL: '',
+      redirectState: {},
+    };
+  }
+
+  renderRedirectTo = () => {
+    const { redirectTo, redirectToURL, redirectState } = this.state;
+    if (redirectTo) {
+      this.setState({
+        redirectTo: false,
+        redirectToURL: '',
+      });
+      return (
+        <Redirect to={{
+          pathname: redirectToURL,
+          state: redirectState,
+        }}
+        />
+      );
+    }
+  }
+
+  handleCategoryClick(category, subcategory) {
+    const selectedCategory = category;
+    this.setState({
+      redirectTo: true,
+      redirectToURL: '/services',
+      redirectState: {
+        category: selectedCategory,
+        subcategory,
+      },
+    });
   }
 
   render() {
-    const { location, classes } = this.props;
+    const { classes } = this.props;
+
     return (
       <React.Fragment>
         <div>
           <h1>Service Categories</h1>
           <p>Select a category that you are looking for</p>
         </div>
-        <ServiceCategories classes={classes} />
+        <Grid container className={classes.root} spacing={40}>
+        {this.renderRedirectTo()}
+          <Grid item xs={12}>
+            <Grid
+              container
+              className={classes.demo}
+              justify="center"
+              spacing={40}
+            >
+              {serviceCategories.map(category => (
+                <Grid key={category.id} item>
+                <CardActionArea>
+                  <CardMedia
+                    className={classes.media}
+                    image="/static/images/cards/contemplative-reptile.jpg"
+                    title="Contemplative Reptile"
+                  />
+                  <Card 
+                  className={classes.card}
+                  onClick={() => this.handleCategoryClick(category.value, '')}
+                  >
+
+                      <CardContent>
+                        <Typography component="p">{category.label}</Typography>
+                      </CardContent>
+                  </Card>
+                </CardActionArea>
+                </Grid>
+              ))}
+            </Grid>
+          </Grid>
+        </Grid>
       </React.Fragment>
     );
   }
@@ -31,4 +110,4 @@ ServiceCategoryList.propTypes = {
   location: PropTypes.shape({}).isRequired,
 };
 
-export default ServiceCategoryList;
+export default withStyles(styles)(ServiceCategoryList);
