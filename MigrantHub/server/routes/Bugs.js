ebca968c6627885c65d34b7ec3c16cd5c9911dc3
rajee10 <rvairam10@gmail.com>
2019-03-29 23:17:19 -0400

    #241 Added server side functionality to retrieve bugs from database

--- MigrantHub/server/routes/Bugs.js
+++ MigrantHub/server/routes/Bugs.js
@@ -5,5 +5,7 @@ const BugController = require('../controllers/BugController');
 const { controllerHandler } = require('../controllers/ControllerUtils');
 
 router.post('/', controllerHandler(BugController.createBug, req => [req.user, req.body]));
+router.get('/', controllerHandler(BugController.getBugs, () => []));
+router.get('/:id', controllerHandler(BugController.getBugs, req => [req.query._id]));
 
 module.exports = router;
